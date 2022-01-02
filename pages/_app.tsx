d512627084ce4f83e3d0870337d6403a3d9bac30
Agustin Sandoval <agusandoval12@gmail.com>
2022-01-02 00:42:11 -0300

    Se agregan todos los archivos al repo, se crea rama Master

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import RecipeApp from "./components/RecipeApp";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <ChakraProvider>
+            <Component {...pageProps} />
+            {/* <RecipeApp /> */}
+        </ChakraProvider>
+    );
 }
 
-export default MyApp
+export default MyApp;
